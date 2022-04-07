24c3e154ff64cf5b94d1223eda7288d1ba3dd459
Boris Stankov <103064087+BorisStankov98@users.noreply.github.com>
2022-04-07 10:56:09 +0300

    Update app.js

--- Character counter/app.js
+++ Character counter/app.js
@@ -1,12 +1,17 @@
 const lettersBox = document.getElementById("letters")
 const textArea =document.getElementById("textarea")
+const totalChars = document.getElementById("charCounter")
+let total = 0;
 let letters={};
 let ch;
 textArea.addEventListener("input", ()=>{
     let text = textArea.value
     let textToLowerCase = text.toLowerCase()
     letters ={};
+    total = 0;
     for(let i=0;i<text.length;i++){
+        total++
+        totalChars.innerHTML= total;
         ch=textToLowerCase[i];
         if(letters[ch] === undefined){
             letters[ch] =1;
@@ -17,8 +22,8 @@ textArea.addEventListener("input", ()=>{
     }
     lettersBox.innerHTML = ""
     for(let letter in letters){
-        const newParagraph = document.createElement("p")
-        newParagraph.append(`${letter}: ${letters[letter]}`)
-        lettersBox.append(newParagraph)
+        const newParagraph = document.createElement("p");
+        newParagraph.append(`${letter}: ${letters[letter]}`);
+        lettersBox.append(newParagraph);
     }
 })
